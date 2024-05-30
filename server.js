93761954a70b421f492847c80e4c9706a6620032
Daria Luneva <daria.safri@gmail.com>
2024-05-30 14:32:20 -0400

    added all existing by now files
    Started work with mongoDB

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+// console.log(app.get('env'));
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
